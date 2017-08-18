eda378e166b37d0950cff53f3dd5155f10ed3131
Christian Najera <chris_najera@unitec.edu>
2017-08-17 23:09:25 -0600

    test finished

--- src/app.js
+++ src/app.js
@@ -1,4 +1,14 @@
-const app = () => {}
+const app = (numbers) => {
+  if (numbers.trim().length == 0) {
+    return 0;
+  }
+  if (numbers.trim().split(',').length < 2) {
+    return numbers.trim();
+  }
+  if (numbers.trim().split(',').length > 1) {
+    return parseInt(numbers.trim().split(',')[0]) + parseInt(numbers.trim().split(',')[1]);
+  }
+}
 module.exports = {
   app
 };
--- test/app.test.js
+++ test/app.test.js
@@ -3,9 +3,21 @@ const mocha = require('mocha');
 
 const app = require('../src/app').app;
 
-describe('testing app.js', () => {
+describe('add', () => {
   it('should return 0 when no numbers are sent', () => {
-    let sum = app();
+    const sum = app('');
     expect(sum).equal(0);
-  })
+  });
+  it('should return the same number when only one number is sent', () => {
+    const digit = parseInt(Math.random() * (0 - 25) + 25);
+    const sum = app(digit.toString());
+    expect(sum).equal(digit.toString());
+  });
+  it('should return the sum when two numbers are sent', () => {
+    const digit1 = parseInt(Math.random() * (0 - 25) + 25);
+    const digit2 = parseInt(Math.random() * (0 - 25) + 25);
+
+    const sum = app(`${digit1},${digit2}`);
+    expect(sum).equal(digit1+digit2);
+  });
 });
